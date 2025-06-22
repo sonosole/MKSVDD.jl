7cbd09d9c8d6d56b95a75dc0255faf0f02155337
Sole <30993566+sonosole@users.noreply.github.com>
2025-06-22 23:28:57 +0800

    config: exports
    SVDD + svddlabel + some kernels

--- src/MKSVDD.jl
+++ src/MKSVDD.jl
@@ -8,7 +8,24 @@ using COSMO
 import LinearAlgebra: diag
 
 include("kernel/inc.jl")
+export ChebyK
+export CityK
+export CosK
+export EucK
+export HammingK
+export JaccardK
+export KLDivK
+export MAEK
+export MSEK
+export MinkowskiK
+export NRMSEK
+export RBFK
+export RMSEK
+export kmat
+
 include("SVDD.jl")
+export SVDD
+export svddlabel
 
 
 end # module MKSVDD
